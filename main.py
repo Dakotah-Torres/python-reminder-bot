d649c936eb8e1c64fe453da9db8a0f68142a7c00
Dakotah-Torres <dtorres@dtdevtech.com>
2025-06-15 15:32:06 -0500

    I changed direction. The email to SMS was not working and too much red tape. So i moved to creating a telegram bot. This add a little more flexability.

--- main.py
+++ main.py
@@ -1,27 +1,65 @@
 import os
-import smtplib
-from email.message import EmailMessage
-
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
-
-load_dotenv() # This will load my enviornment variables
-
-email_address       = os.getenv("MY_EMAIL")
-email_app_pass      = os.getenv("MY_APP_PASSWORD")
-my_phone_number     = os.getenv("MY_PHONE_NUMBER")
-to_number           = my_phone_number+"@tmomail.net"
-message_body        = "This is my first test message"
-
-msg = EmailMessage()
-msg.set_content(message_body)
-msg['From'] = email_address
-msg["To"] = to_number
-
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    print("Logging in with:", email_address)
-    smtp.login(email_address,email_app_pass)
-    smtp.send_message(msg)
+import time
 
 
 
-print("Sent the message")
+load_dotenv(dotenv_path=".env") # This will load my enviornment variables
+BOT_TOKEN = os.getenv("TELE_TOKEN")
+DAILY_TEXT="https://www.jw.org/finder?srcid=jwlshare&wtlocale=E&prefer=lang&alias=daily-text"
+
+time_reminders = {}
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await time_loop(update, context)
+    await update.message.reply_text(f"Welcome To Your Reminder  Bot!")
+
+async def add(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    reminder = " ".join(context.args)
+    await update.message.reply_text(f"Reminder Added: {reminder}")
+
+async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    daily_text = DAILY_TEXT
+    print(daily_text)
+    await update.message.reply_text(daily_text)
+
+async def clean_chat(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    current_id = update.message.message_id
+
+    for msg_id in range(current_id - 30, current_id):
+        try:
+            await context.bot.delete_message(chat_id=chat_id, message_id=msg_id)
+        except:
+            pass
+
+async def time_based_reminder(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    args = context.args
+    time = args[0]
+    reminder = " ".join(args[1:])
+    time_reminders.update({time: reminder})
+    print(time_reminders)
+    await update.message.reply_text("Reminder added")
+
+async def time_loop(Update, Context):
+    while True:
+        current_time = time.strftime("%H:%M")
+        for ti in time_reminders:
+            if ti == current_time:
+                await Update.message.reply_text(time_reminders[ti])
+            else:
+                pass
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("add", add))
+app.add_handler(CommandHandler("text", text))
+app.add_handler(CommandHandler("clean", clean_chat))
+app.add_handler(CommandHandler("time", time_based_reminder))
+
+app.run_polling()
+
+# bot_name: dypyrembot
+
